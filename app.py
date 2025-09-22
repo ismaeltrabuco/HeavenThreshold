4e9cae6d858a3a3e59991d6e28208c49a1ba203b
Ismael Trabuco <ismaeltrabuco@gmail.com>
2025-09-22 18:01:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scikitlearn.linear_model import LogisticRegression
-
-st.set_page_config(page_title="✨ Heaven Threshold ✨", layout="wide")
+from sklearn.linear_model import LogisticRegression
 
 st.title("✨ Heaven Threshold ✨")
 st.write("""
